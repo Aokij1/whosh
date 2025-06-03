ebcd8c795b1eb7ed87f7e0056bc84767b1fc0470
Aokiji <110603525+Aokij1@users.noreply.github.com>
2025-06-03 15:40:30 +0300

    Update reg.py

--- Whoosh/regedit/reg.py
+++ Whoosh/regedit/reg.py
@@ -1,69 +1,99 @@
 import winreg, os
 from PyQt5.QtWidgets import QMessageBox
 
+def warning(text):
+    message = QMessageBox()
+    message.setWindowTitle("Ошибка!")
+    message.setText(text)
+    message.setIcon(QMessageBox.Information)
+    message.exec_()
 
 # Получение доступа
-def dostup(self):
-    # Добавление значений
-    register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
-                                  'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System', 0, winreg.KEY_WRITE)
-    winreg.SetValueEx(register_key, 'ConsentPromptBehaviorAdmin', 0, winreg.REG_DWORD, 5)
-    winreg.SetValueEx(register_key, 'ConsentPromptBehaviorUser', 0, winreg.REG_DWORD, 3)
-    winreg.SetValueEx(register_key, 'DSCAutomationHostEnabled', 0, winreg.REG_DWORD, 2)
-    winreg.SetValueEx(register_key, 'EnableCursorSuppression', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'EnableInstallerDetection', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'EnableLUA', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'EnableSecureUIAPaths', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'EnableUIADesktopToggle', 0, winreg.REG_DWORD, 0)
-    winreg.SetValueEx(register_key, 'EnableVirtualization', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'PromptOnSecureDesktop', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'ValidateAdminCodeSignatures', 0, winreg.REG_DWORD, 0)
-    winreg.SetValueEx(register_key, 'dontdisplaylastusername', 0, winreg.REG_DWORD, 0)
-    winreg.SetValueEx(register_key, 'legalnoticecaption', 0, winreg.REG_SZ, '')
-    winreg.SetValueEx(register_key, 'legalnoticetext', 0, winreg.REG_SZ, '')
-    winreg.SetValueEx(register_key, 'scforceoption', 0, winreg.REG_DWORD, 0)
-    winreg.SetValueEx(register_key, 'shutdownwithoutlogon', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'undockwithoutlogon', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'LocalAccountTokenFilterPolicy', 0, winreg.REG_DWORD, 1)
+def dostup():
+    try:
+        # Добавление значений
+        register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                      'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System', 0, winreg.KEY_WRITE)
+        winreg.SetValueEx(register_key, 'ConsentPromptBehaviorAdmin', 0, winreg.REG_DWORD, 5)
+        winreg.SetValueEx(register_key, 'ConsentPromptBehaviorUser', 0, winreg.REG_DWORD, 3)
+        winreg.SetValueEx(register_key, 'DSCAutomationHostEnabled', 0, winreg.REG_DWORD, 2)
+        winreg.SetValueEx(register_key, 'EnableCursorSuppression', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'EnableInstallerDetection', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'EnableLUA', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'EnableSecureUIAPaths', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'EnableUIADesktopToggle', 0, winreg.REG_DWORD, 0)
+        winreg.SetValueEx(register_key, 'EnableVirtualization', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'PromptOnSecureDesktop', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'ValidateAdminCodeSignatures', 0, winreg.REG_DWORD, 0)
+        winreg.SetValueEx(register_key, 'dontdisplaylastusername', 0, winreg.REG_DWORD, 0)
+        winreg.SetValueEx(register_key, 'legalnoticecaption', 0, winreg.REG_SZ, '')
+        winreg.SetValueEx(register_key, 'legalnoticetext', 0, winreg.REG_SZ, '')
+        winreg.SetValueEx(register_key, 'scforceoption', 0, winreg.REG_DWORD, 0)
+        winreg.SetValueEx(register_key, 'shutdownwithoutlogon', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'undockwithoutlogon', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'LocalAccountTokenFilterPolicy', 0, winreg.REG_DWORD, 1)
+    
+        # Создание новых каталогов
+        winreg.CreateKey(register_key, 'Audit')
+        winreg.CreateKey(register_key, 'UIPI')
+        register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                      'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\UIPI', 0,
+                                      winreg.KEY_WRITE)
+        winreg.CreateKey(register_key, 'Clipboard')
+        register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                      'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\UIPI\\Clipboard', 0,
+                                      winreg.KEY_WRITE)
+        winreg.CreateKey(register_key, 'ExceptionFormats')
+    
+        # Добавление значений
+        register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                                      'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\UIPI\\Clipboard\\ExceptionFormats',
+                                      0, winreg.KEY_WRITE)
+        winreg.SetValueEx(register_key, 'CF_BITMAP', 0, winreg.REG_DWORD, 2)
+        winreg.SetValueEx(register_key, 'CF_DIB', 0, winreg.REG_DWORD, 8)
+        winreg.SetValueEx(register_key, 'CF_DIBV5', 0, winreg.REG_DWORD, 17)
+        winreg.SetValueEx(register_key, 'CF_OEMTEXT', 0, winreg.REG_DWORD, 7)
+        winreg.SetValueEx(register_key, 'CF_PALETTE', 0, winreg.REG_DWORD, 9)
+        winreg.SetValueEx(register_key, 'CF_TEXT', 0, winreg.REG_DWORD, 1)
+        winreg.SetValueEx(register_key, 'CF_UNICODETEXT', 0, winreg.REG_DWORD, 0x0000000d)
 
-    # Создание новых каталогов
-    winreg.CreateKey(register_key, 'Audit')
-    winreg.CreateKey(register_key, 'UIPI')
-    register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
-                                  'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\UIPI', 0,
-                                  winreg.KEY_WRITE)
-    winreg.CreateKey(register_key, 'Clipboard')
-    register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
-                                  'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\UIPI\\Clipboard', 0,
-                                  winreg.KEY_WRITE)
-    winreg.CreateKey(register_key, 'ExceptionFormats')
-
-    # Добавление значений
-    register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
-                                  'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\UIPI\\Clipboard\\ExceptionFormats',
-                                  0, winreg.KEY_WRITE)
-    winreg.SetValueEx(register_key, 'CF_BITMAP', 0, winreg.REG_DWORD, 2)
-    winreg.SetValueEx(register_key, 'CF_DIB', 0, winreg.REG_DWORD, 8)
-    winreg.SetValueEx(register_key, 'CF_DIBV5', 0, winreg.REG_DWORD, 17)
-    winreg.SetValueEx(register_key, 'CF_OEMTEXT', 0, winreg.REG_DWORD, 7)
-    winreg.SetValueEx(register_key, 'CF_PALETTE', 0, winreg.REG_DWORD, 9)
-    winreg.SetValueEx(register_key, 'CF_TEXT', 0, winreg.REG_DWORD, 1)
-    winreg.SetValueEx(register_key, 'CF_UNICODETEXT', 0, winreg.REG_DWORD, 0x0000000d)
+        warning(text='Выполнено')
+    
+    except PermissionError:
+        warning(text='Недостаточно прав для записи в реестр. Запустите скрипт с правами администратора.')
+    except FileNotFoundError as e:
+        warning(text=f'Указанный ключ реестра не найден: \n{e}')
+    except OSError as e:
+        warning(text=f'Ошибка работы с реестром: \n{e}')
+    except CalledProcessError as e:
+        warning(text=f'Непредвиденная ошибка: \n{e}')
 
 # Отключаю автоматическое устранение неполадок и ее оповещение через политику
-def ne_poladka(self):
-    register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Microsoft\\Windows\\Windows Error Reporting' ,0, winreg.KEY_WRITE)
-    winreg.SetValueEx(register_key, 'Disabled', 0, winreg.REG_DWORD, 1)
+def ne_poladka():
+    try:
+        register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Microsoft\\Windows\\Windows Error Reporting' ,0, winreg.KEY_WRITE)
+        winreg.SetValueEx(register_key, 'Disabled', 0, winreg.REG_DWORD, 1)
+    
+        register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Policies\\Microsoft\\Windows',0,winreg.KEY_WRITE)
+        winreg.CreateKey(register_key, 'ScriptedDiagnostics')
+    
+        register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Policies\\Microsoft\\Windows\\ScriptedDiagnostics' , 0, winreg.KEY_WRITE)
+        winreg.SetValueEx(register_key, 'EnableDiagnostics', 0, winreg.REG_DWORD, 0)
+    
+        winreg.CreateKey(register_key, 'Policy')
+        register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Policies\\Microsoft\\Windows\\ScriptedDiagnostics\\Policy' , 0, winreg.KEY_WRITE)
+        winreg.SetValueEx(register_key, 'EnableDiagnostics', 0, winreg.REG_DWORD, 1)
 
-    register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Policies\\Microsoft\\Windows',0,winreg.KEY_WRITE)
-    winreg.CreateKey(register_key, 'ScriptedDiagnostics')
-
-    register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Policies\\Microsoft\\Windows\\ScriptedDiagnostics' , 0, winreg.KEY_WRITE)
-    winreg.SetValueEx(register_key, 'EnableDiagnostics', 0, winreg.REG_DWORD, 0)
-
-    winreg.CreateKey(register_key, 'Policy')
-    register_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\Policies\\Microsoft\\Windows\\ScriptedDiagnostics\\Policy' , 0, winreg.KEY_WRITE)
-    winreg.SetValueEx(register_key, 'EnableDiagnostics', 0, winreg.REG_DWORD, 1)
+        warning(text='Выполнено')
+        
+    except PermissionError:
+        warning(text='Недостаточно прав для записи в реестр. Запустите скрипт с правами администратора.')
+    except FileNotFoundError as e:
+        warning(text=f'Указанный ключ реестра не найден: \n{e}')
+    except OSError as e:
+        warning(text=f'Ошибка работы с реестром: \n{e}')
+    except CalledProcessError as e:
+        warning(text=f'Непредвиденная ошибка: \n{e}')
 
 #Скрытые папки
 def data_invis(self):
