52bd5163e1809b3b5aa965525ca2a74787e481c5
Aokiji <110603525+Aokij1@users.noreply.github.com>
2025-06-18 17:53:31 +0300

    Update inst.py

--- Whoosh/install_programm/inst.py
+++ Whoosh/install_programm/inst.py
@@ -1,7 +1,12 @@
 from PyQt5.QtWidgets import QMessageBox
 import os, subprocess, shutil
 
+install_path = ""
 
+def do_install(path):
+    global install_path
+    install_path = path
+    print(install_path)
 
 def processing(text):
     message = QMessageBox()
@@ -10,136 +15,289 @@ def processing(text):
     message.setIcon(QMessageBox.Information)
     message.exec_()
 
+# Окно информации Ошибка или Выполнено
+def warning(text):
+    message = QMessageBox()
+    message.setWindowTitle("Внимание!")
+    message.setText(text)
+    message.setIcon(QMessageBox.Information)
+    message.exec_()
 
+# Окно предупреждения что будет если нажать на кнопку
+def message_warning(text):
+    message = QMessageBox()
+    message.setWindowTitle("Предупреждение")
+    message.setText(text)
+    message.setIcon(QMessageBox.Warning)
+    message.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+
+    result = message.exec_()
+    return result
+
+# Устанавливаем Adobe
 def adob():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', 'AcroRdrDC2100520060_MUI.exe')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки Adobe. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
+
+        exe_path = (install_path + '/AcroRdrDC2100520060_MUI.exe')
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+    elif result == QMessageBox.Cancel:
+        return
+
+# Устанавливаем Free_vimage
 def free_vimag():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', 'FreeVimager-9.9.19-Setup-Rus.exe')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки Free Vimage. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
+
+        exe_path = (install_path +  '/FreeVimager-9.9.19-Setup-Rus.exe')
+
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+
+    elif result == QMessageBox.Cancel:
+        return
+#
+
+# Устанавливаем 7Zip
 def zip():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', '7z2301-x64.exe')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки 7Zip. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
+        exe_path = (install_path + '/7z2301-x64.exe')
+
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+
+    elif result == QMessageBox.Cancel:
+            return
+#
+
+# Устанавливаем Sumatra
 def sumart():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', 'SumatraPDF-3.2-64-install.exe')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки Sumatra. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
+
+        exe_path = (install_path + '/SumatraPDF-3.2-64-install.exe')
+
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+
+    elif result == QMessageBox.Cancel:
+        return
+#
+
+# Установка java и копирование файла javaw.exe в C:\Windows\ и C:\Windows\System32\
 def java():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', 'jre-8u311-windows-x64.exe')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки java. \nКопируем файл "javaw.exe" в "C:\Windows\" и "C:\Windows\System32\" \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            result = subprocess.run([exe_path],  check=True)
-            if result.returncode == 0:
-            # Копирование файла
-                url_1 = 'C:\\Program Files\\Java\\jre1.8.0_311\\bin\\javaw.exe'
-                url_2 = 'C:\\Windows\\'
-                url_3 = 'C:\\Windows\\System32\\'
-                try:
-                    result_1 = subprocess.run(shutil.copy(url_1, url_2))
-                    result_2 = subprocess.run(shutil.copy(url_1, url_3))
-                    processing(text='Установка и копироване выполнено!')
-                except subprocess.CalledProcessError as e:
-                    processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
-            else:
-                processing(text='Копирование не выполнено!')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найден')
 
+        exe_path = (install_path + '/jre-8u311-windows-x64.exe')
+
+        if os.path.exists(exe_path):
+            try:
+                result = subprocess.run([exe_path],  check=True)
+                if result.returncode == 0:
+                # Копирование файла
+                    url_1 = 'C:\\Program Files\\Java\\jre1.8.0_311\\bin\\javaw.exe'
+                    url_2 = 'C:\\Windows\\'
+                    url_3 = 'C:\\Windows\\System32\\'
+                    try:
+                        result_1 = subprocess.run(shutil.copy(url_1, url_2))
+                        result_2 = subprocess.run(shutil.copy(url_1, url_3))
+                        processing(text='Установка и копироване выполнено!')
+                    except subprocess.CalledProcessError as e:
+                        processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+
+                else:
+                    processing(text='Копирование не выполнено!')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найден')
+
+
+
+    elif result == QMessageBox.Cancel:
+        return
+#
+
+# Установка КОМПАСА
 def komp():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', '3D_Viewer_V16_x64.msi')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки КОМПАСА. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
+
+        exe_path = (install_path + '/3D_Viewer_V16_x64.msi')
+
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+
+    elif result == QMessageBox.Cancel:
+        return
+#
+
+# Установка PDFSam
 def pdf_sam():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', 'PDFsamBasic4Installer.exe')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки PDFSam. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
+
+        exe_path = (install_path + '/PDFsamBasic4Installer.exe')
+
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+
+    elif result == QMessageBox.Cancel:
+        return
+#
+
+# Установка Google Chrome
 def chrom():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', 'ChromeStandaloneSetup64.exe')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки Google Chrome. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
+
+        exe_path = (install_path + '/ChromeStandaloneSetup64.exe')
+
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+
+    elif result == QMessageBox.Cancel:
+        return
+#
+
+# Установка FrameWork
 def framework():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', 'dotnetfx35.exe')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки PDFSam. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
 
+
+        exe_path = (install_path + '/dotnetfx35.exe')
+
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+
+    elif result == QMessageBox.Cancel:
+        return
+#
+
+# Установка DWRCS
 def dame():
-    exe_path = os.path.join(os.path.abspath(os.curdir), '_internal', 'DWRCSVista32.MSI')
+    # Получаем значение из окна предупреждений попутно вызывая его с текстом
+    result = message_warning('Запустит файл установки PDFSam. \n(ПрогрессБар не будет отображаться)')
 
-    if os.path.exists(exe_path):
-        try:
-            subprocess.run([exe_path], check=True)
-            processing(text='Установка выполнена')
-        except subprocess.CalledProcessError as e:
-            processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
-    else:
-        processing(text='Файл не найдет')
+    # Проверяем что нажал пользовтель
+    if result == QMessageBox.Ok:
+
+
+        exe_path = (install_path + '/DWRCSVista32.MSI')
+
+        if os.path.exists(exe_path):
+            try:
+                subprocess.run([exe_path], check=True)
+                processing(text='Установка выполнена')
+            except subprocess.CalledProcessError as e:
+                processing(text=f'Ошибка при выполнении команды \n{e} \nКод возврата: {e.returncode}')
+        else:
+            processing(text='Файл не найдет')
+
+
+
+    elif result == QMessageBox.Cancel:
+        return
